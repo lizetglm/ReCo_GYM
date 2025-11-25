b431f284f0f22deb8a164dea57920007b2b007ae
lizetglm <lizetglm@gmail.com>
2025-11-25 15:31:46 -0600

    Implementación de cors en puertos locales

--- ReCo_Gym/settings.py
+++ ReCo_Gym/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -141,3 +143,8 @@ REST_FRAMEWORK = {
 }
 
 LOGIN_URL = 'login_usuario'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
