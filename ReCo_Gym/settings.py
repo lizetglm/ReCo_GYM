0cea7f00a2314d2f239dc63d427e7aa41a46db4e
lizetglm <lizetglm@gmail.com>
2025-11-28 11:30:30 -0600

    aa

--- ReCo_Gym/settings.py
+++ ReCo_Gym/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'djkncnjrkehhtw4intvo3ipvri34iur348urihferuh3948cpc9bcniyn87t@@DJHJHufiu#!@Wjjkll'
+#SECRET_KEY = 'djkncnjrkehhtw4intvo3ipvri34iur348urihferuh3948cpc9bcniyn87t@@DJHJHufiu#!@Wjjkll'
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
